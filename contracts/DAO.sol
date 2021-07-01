4e8e97fb6e03f41d0240d580dc0e6f8ad5cc3302
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2021-07-01 11:12:32 +0200

    ERC777 OK

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,56 +1,93 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
+import "./Gouverno.sol";
 
-contract DAO {
+//1000000000000000000000
+
+contract DAO is IERC777Recipient {
     using Counters for Counters.Counter;
-    
-    enum Vote {Yes, No}
-    enum Status {Running, Approved, Rejected}
-    
+
+    enum Vote {
+        Yes,
+        No
+    }
+    enum Status {
+        Running,
+        Approved,
+        Rejected
+    }
+
     struct Proposal {
         Status status;
-        address author;
+        address proposer;
         uint256 createdAt;
         uint256 nbYes;
         uint256 nbNo;
         string proposition;
     }
-    
-    uint256 constant public TIME_LIMIT = 3 minutes;
-    
+
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
+    uint256 public constant TIME_LIMIT = 3 minutes;
+    IERC777 private _gouverno;
     Counters.Counter private _id;
     mapping(uint256 => Proposal) private _proposals;
     mapping(address => mapping(uint256 => bool)) private _hasVote;
-    
-    
-    function createProposal(string memory proposition) public returns(uint256) {
+    mapping(address => uint256) private _votesBalances;
+
+    constructor() {
+        _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
+        address[] memory defaultOperators_ = new address[](1);
+        defaultOperators_[0] = address(this);
+        _gouverno = new Gouverno(defaultOperators_);
+    }
+
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external override {}
+
+    function deposit(uint256 amount) public {
+        _votesBalances[msg.sender] += amount;
+        _gouverno.operatorSend(msg.sender, address(this), amount, "", "");
+    }
+
+    function withdraw(uint256 amount) public {}
+
+    function propose(string memory proposition) public returns (uint256) {
         _id.increment();
         uint256 id = _id.current();
         _proposals[id] = Proposal({
             status: Status.Running,
-            author: msg.sender,
+            proposer: msg.sender,
             createdAt: block.timestamp,
-            nbYes: 0, 
+            nbYes: 0,
             nbNo: 0,
             proposition: proposition
         });
         return id;
-        
     }
-    
+
     function vote(uint256 id, Vote vote_) public {
         require(_hasVote[msg.sender][id] == false, "DAO: Already voted");
         require(_proposals[id].status == Status.Running, "DAO: Not a running proposal");
-        
-        if(block.timestamp > _proposals[id].createdAt + TIME_LIMIT) {
-            if(_proposals[id].nbYes > _proposals[id].nbNo) {
+
+        if (block.timestamp > _proposals[id].createdAt + TIME_LIMIT) {
+            if (_proposals[id].nbYes > _proposals[id].nbNo) {
                 _proposals[id].status = Status.Approved;
             } else {
                 _proposals[id].status = Status.Rejected;
             }
         } else {
-            if(vote_ == Vote.Yes) {
+            if (vote_ == Vote.Yes) {
                 _proposals[id].nbYes += 1;
             } else {
                 _proposals[id].nbNo += 1;
@@ -58,12 +95,20 @@ contract DAO {
             _hasVote[msg.sender][id] = true;
         }
     }
-    
-    function proposalById(uint256 id) public view returns(Proposal memory) {
+
+    function proposalById(uint256 id) public view returns (Proposal memory) {
         return _proposals[id];
     }
-    
-    function hasVote(address account, uint256 id) public view returns(bool) {
+
+    function hasVote(address account, uint256 id) public view returns (bool) {
         return _hasVote[account][id];
+    }
+
+    function gouverno() public view returns (address) {
+        return address(_gouverno);
+    }
+
+    function votesBalanceOf(address account) public view returns (uint256) {
+        return _votesBalances[account];
     }
 }
